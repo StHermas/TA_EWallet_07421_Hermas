3102ffbd3f86eb74bbae513ed4e5b90cffadb04a
unknown <hermasbintang@gmail.com>
2023-01-01 16:19:12 +0700

    Update MVC & Final CLI

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import controller.AuthController;
+import view.AuthPage.LoginView;
+
+public class Main {
+    public static void main(String[] args) {
+        new AuthController().toViewL();
+    }
+}
